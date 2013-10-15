063904d7c2b2d8cf884ec0cc2ccba33ee462a9e4
Ralph Tan <r.tan@klab.com>
2013-10-15 12:49:56 +0800

    Re-pushing user_controller to dev_1.0

--- app/controllers/user_controller.php
+++ app/controllers/user_controller.php
@@ -9,5 +9,64 @@
 
 class UserController extends AppController
 {
+    public function login()
+    {
+        $user = new User;
+        $page = Param::get('page_next', 'login');
 
+        switch($page) {
+            case 'login':
+                break;
+            case 'login_success':
+                $user->username = Param::get('username');
+                $user->password = Param::get('password');
+                try {
+                    $page = $user->login();
+                } catch (ValidationException $e) {
+                    $page = 'login';
+                }
+                break;
+            default:
+                throw new NotFoundException("{$page} is not found");
+                break;
+        }
+
+        $this->set(get_defined_vars());
+        $this->render($page);
+    }
+
+    public function register()
+    {
+        $user = new User;
+        $page = Param::get('page_next', 'register');
+
+        switch($page) {
+            case 'register':
+                break;
+            case 'register_end':
+                $user->username = Param::get('username');
+                $user->password = Param::get('password');
+                $user->rep_password = Param::get('rep_pass');
+                try {
+                    $user->register();
+                    $_SESSION['username'] = Param::get('username');
+                } catch(ValidationException $e) {
+                    $page = 'register';
+                }
+                break;
+            default:
+                throw new NotFoundException("{$page} is not found");
+                break;
+        }
+        $this->set(get_defined_vars());
+        $this->render($page);
+    }
+
+    public function logout()
+    {
+        $user = new User;
+        $user->logout();
+        $this->set(get_defined_vars());
+        $this->render($user->page);
+    }
 }
