24d609822e2a179610c42e0e854ec6e55b19f610
Ralph Tan <r.tan@klab.com>
2013-10-15 13:45:37 +0800

    Refresh login_failed.php

--- app/views/user/login_failed.php
+++ app/views/user/login_failed.php
@@ -7,8 +7,3 @@
  * To change this template use File | Settings | File Templates.
  */
 ?>
-<h2>Log In Failed</h2>
-<p class="alert alert-block">
-    Invalid Username or Password
-</p>
-<a href="<?php eh(url('user/login'))?>">Please try again.</a>
